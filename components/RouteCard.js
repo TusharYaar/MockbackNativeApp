38911dcf0e521fffba522fe8a796faea52a97e96
Tushar Agrawal <tusharsagrawal16@gmail.com>
2021-08-08 21:55:17 +0530

    route open in browser

--- components/RouteCard.js
+++ components/RouteCard.js
@@ -1,25 +1,41 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Dimensions } from "react-native";
 
-import { useNavigation } from "@react-navigation/native";
-import Accordion from "./Accordion";
+import { useNavigation,useTheme } from "@react-navigation/native";
+import * as WebBrowser from 'expo-web-browser';
 
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { Card, Subheading, Text } from "react-native-paper";
+import { Text,IconButton } from "react-native-paper";
 
 import { RESPONSE } from "../data/mappings";
 import Button from "../components/Button";
+import Accordion from "./Accordion";
+
+
+
+const windowWidth = Dimensions.get('window').width;
 const RouteCard = ({ route, mockspace }) => {
   const navigation = useNavigation();
+  const {colors} = useTheme();
+
+  const openLink = () => {
+    WebBrowser.openBrowserAsync(`https://mockback.herokuapp.com/${mockspace}/r/${route.pathname}`);
+  }
+  
   return (
     <Accordion
       title={`/${route.pathname}`}
       icon={route.authorization ? "lock" : "lock-open-variant"}
       style={styles.card}
     >
+      <View style={styles.urlContainer}>
       <Text>
         {`https://mockback.herokuapp.com/${mockspace}/r/` + route.pathname}
       </Text>
+      <IconButton icon="monitor-share"
+      color={colors.accent}
+    size={20}
+    onPress={openLink} />
+      </View>
       <Text>
         {route.method} {route.httpStatus}
       </Text>
@@ -52,4 +68,11 @@ const styles = StyleSheet.create({
     alignSelf: "flex-end",
     marginVertical: 10,
   },
+  urlContainer:{
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 5, 
+    width: windowWidth - 120
+  }
 });
